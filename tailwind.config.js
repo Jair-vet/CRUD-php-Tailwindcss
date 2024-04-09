baa05094f558c6336d40750b7fef781e41a60fca
Jair <jairaceves56@gmail.com>
2024-04-09 17:12:33 -0600

    Instalamos tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    "./index.php",
+    "./src/**/*.{vue,js,ts,jsx,tsx,php}",
+    "./**/*.{vue,js,ts,jsx,tsx,php}",
+    "../includes/**/*.php",
+    "../auth/**/*.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
